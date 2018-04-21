0c8f663990533ad3411cc35003def5f7611ed9b8
Glen Willis <glenwillis@gmail.com>
2018-04-21 17:59:22 +0800

    Resets the sound bar properly now.

--- Assets/Scripts/RythmGame/SoundBarMovement.cs
+++ Assets/Scripts/RythmGame/SoundBarMovement.cs
@@ -7,8 +7,18 @@ public class SoundBarMovement : MonoBehaviour {
     [Range(0.0F, 10.0F)]
     public float SpeedOfMovement;
 
+    public Transform StartPoint;
+
 	void Update ()
     {
         transform.Translate(new Vector3(1F, 0F, 0F) * SpeedOfMovement);
 	}
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if(coll.gameObject.tag == "ResetPoint")
+        {
+            transform.position = StartPoint.position;
+        }
+    }
 }
