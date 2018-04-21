4117c39d97265194461f8cbee24698a8c491fb7d
Glen Willis <glenwillis@gmail.com>
2018-04-21 22:03:49 +0800

    Have some general texts included.

--- Assets/Scripts/GameControl/GameController.cs
+++ Assets/Scripts/GameControl/GameController.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
-    public int NumberOfEnemiesLeft;
-    public int CurrentRoundNumber;
+    private int _numberOfEnemiesLeft;
+    private int _currentRoundNumber;
 
     private int _numberOfLivesLeft;
+    private float _moneyLeft;
     public bool GameOn;
+    GameText _gameText;
 
     public int NumberOfLivesLeft
     {
@@ -20,10 +22,53 @@ public class GameController : MonoBehaviour {
         set
         {
             _numberOfLivesLeft = value;
+            _gameText.LivesTextUpdate();
             if (_numberOfLivesLeft <= 0)
             {
                 StartCoroutine("LevelFailed");
             }
+        }
+    }
+
+    public int NumberOfEnemiesLeft
+    {
+        get
+        {
+            return _numberOfEnemiesLeft;
+        }
+
+        set
+        {
+            _numberOfEnemiesLeft = value;
+            _gameText.EnemiesLeftTextUpdate();
+        }
+    }
+
+    public int CurrentRoundNumber
+    {
+        get
+        {
+            return _currentRoundNumber;
+        }
+
+        set
+        {
+            _currentRoundNumber = value;
+            _gameText.RoundTextUpdate();
+        }
+    }
+
+    public float MoneyLeft
+    {
+        get
+        {
+            return _moneyLeft;
+        }
+
+        set
+        {
+            _moneyLeft = value;
+            _gameText.MoneyTextUpdate();
         }
     }
 
---
+++ Assets/Scripts/GameControl/GameText.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public class GameText : MonoBehaviour {
+
+    public TextMeshPro MoneyText, RoundText, LivesText, EnemiesLeftText;
+
+    GameController _gameController;
+
+    void Start()
+    {
+        _gameController = GetComponent<GameController>();
+        MoneyTextUpdate();
+        RoundTextUpdate();
+        LivesTextUpdate();
+        EnemiesLeftTextUpdate();
+    }
+
+    public void MoneyTextUpdate()
+    {
+        MoneyText.text = string.Format("Money: {0}", _gameController.MoneyLeft);
+    }
+
+    public void RoundTextUpdate()
+    {
+        RoundText.text = string.Format("Round: {0}", _gameController.CurrentRoundNumber);
+    }
+
+    public void LivesTextUpdate()
+    {
+        LivesText.text = string.Format("Lives: {0}", _gameController.NumberOfLivesLeft);
+    }
+
+    public void EnemiesLeftTextUpdate()
+    {
+        LivesText.text = string.Format("Left: {0}", _gameController.NumberOfEnemiesLeft);
+    }
+}
