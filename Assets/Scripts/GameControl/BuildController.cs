71699ff9367eee59c3ff72635169dc193a1965e0
Glen Willis <glenwillis@gmail.com>
2018-04-21 21:19:46 +0800

    Have the start of a build controller system

--- Assets/Scripts/GameControl/BuildController.cs
+++ Assets/Scripts/GameControl/BuildController.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class BuildController : MonoBehaviour {
 
+    private RaycastHit _hit;
+    public Transform CurrentTurretSpawnPoint;
+    private bool _buildMenuUp;
+    public GameObject BuildMenu;
+
 	void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray))
+            if (Physics.Raycast(ray, out _hit, 100.0F))
             {
-
+                if(_hit.transform.tag == "TurrentSpawn")
+                {
+                    CurrentTurretSpawnPoint = _hit.transform;
+                    if (!_buildMenuUp)
+                    {
+                        _buildMenuUp = true;
+                     
+                    }
+                    Debug.Log("Can spawn here");
+                }
+            }
+            else
+            {
+                _buildMenuUp = false;
+                ToggleBuildMenu(_buildMenuUp);
             }
         }
+    }
+
+    void ToggleBuildMenu(bool enable)
+    {
+        if (enable)
+        {
+            if (!BuildMenu.activeSelf)
+            {
+                BuildMenu.SetActive(true);
+            }
+        }
+        else
+        {
+            if (BuildMenu.activeSelf)
+            {
+                BuildMenu.SetActive(false);
+            }
+        }
+        
     }
 }
