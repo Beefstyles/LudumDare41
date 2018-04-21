c127df01a9047f58215f84dd62e3561483a34261
Glen Willis <glenwillis@gmail.com>
2018-04-21 16:06:25 +0800

    Adding in a level reader and populator script to set out the level.

---
+++ Assets/Scripts/LevelControl/LevelPopulator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelPopulator : MonoBehaviour {
+
+    public GameObject StartPoint, EndPoint, RoadBlock, WayPointBlock, TurrentSpawnBlock;
+    public Transform LevelObjectsSpawnParent;
+    public GameObject LevelGrid;
+    private Renderer _levelGridRenderer;
+    private float _xPosition, _yPosition;
+    private Vector2 _objectPosition;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public IEnumerator PopulateLevel(string[] levelText, int gridXSize, int gridYSize)
+    {
+        LevelGrid.transform.localScale = new Vector3(gridXSize, gridYSize, 1F);
+        _levelGridRenderer = LevelGrid.GetComponent<Renderer>();
+        _levelGridRenderer.material.mainTextureScale = new Vector2(gridXSize, gridYSize);
+        for (int i = 0; i < levelText.Length; i++)
+        {
+            char[] levelTextChar = levelText[i].ToCharArray();
+            for (int j = 0; j < levelText.Length; j++)
+            {
+                _xPosition = j - (gridYSize / 2 - 0.5F);
+                _yPosition = (gridYSize / 2 - 0.5F) - i;
+                _objectPosition = new Vector2(_xPosition, _yPosition);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/LevelControl/LevelReader.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using UnityEngine;
+using LitJson;
+
+public class LevelReader : MonoBehaviour {
+
+    private string _jsonLevelData;
+    private string[] _levelArray;
+    private int _gridXSize, _gridYSize;
+    private float _initialMoney;
+    private int _numberOfRounds;
+
+    private LevelPopulator _levelPopulator;
+
+
+    /*
+     * Title": "FirstLevel",
+        "GridXSize": 10,
+        "GridYSize": 20,
+        "Money": 50000.0,
+        "NumberOfRounds":5,
+        "LevelGridArray":
+     */
+
+    public class JSONLevelDataParser
+    {
+        /*
+        * Title": "FirstLevel",
+       "GridXSize": 10,
+       "GridYSize": 20,
+       "Money": 50000.0,
+       "NumberOfRounds":5,
+       "LevelGridArray":
+        */
+        public string Title;
+        public int GridXSize, GridYSize;
+        public float Money;
+        public int NumberOfRounds;
+        public string[] LevelGridArray;
+    }
+
+    void Awake()
+    {
+        try
+        {
+            _jsonLevelData = File.ReadAllText(Application.dataPath + "/Resources/Levels/Level_1.json");
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Error in loading the json level data file: " + e.Message);
+        }
+        ParseJSONLevelDataToClass();
+        _levelPopulator = GetComponent<LevelPopulator>();
+    }
+
+    private void ParseJSONLevelDataToClass()
+    {
+        try
+        {
+            JSONLevelDataParser levelData = JsonMapper.ToObject<JSONLevelDataParser>(_jsonLevelData);
+            _levelArray = levelData.LevelGridArray;
+            _gridXSize = levelData.GridXSize;
+            _gridYSize = levelData.GridYSize;
+            _initialMoney = levelData.Money;
+            _numberOfRounds = levelData.NumberOfRounds;
+        }
+        catch(Exception e)
+        {
+            Debug.LogError("Failed in parsing the JSON data: " + e.Message);
+        }
+    }
+}
+
