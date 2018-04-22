fcab2b2a634bc09e4fac82cb80b7656e908b485d
Glen Willis <glenwillis@gmail.com>
2018-04-22 10:22:15 +0800

    Have ability to select a building spawn point now.

--- Assets/Scripts/GameControl/BuildController.cs
+++ Assets/Scripts/GameControl/BuildController.cs
@@ -12,20 +12,21 @@ public class BuildController : MonoBehaviour {
     private ProjectileTypes _selectedTurretType;
     private GameController _gameController;
     private BuildingInfo _buildingInfo;
+    public GameObject SelectedTurretSpawnLocation;
 
     void Start()
     {
         _gameController = GetComponent<GameController>();
         _buildingInfo = GetComponent<BuildingInfo>();
     }
-	void Update()
+    void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out _hit, 100.0F))
             {
-                if(_hit.transform.tag == "TurrentSpawn")
+                if (_hit.transform.tag == "TurrentSpawn")
                 {
                     CurrentTurretSpawnPoint = _hit.transform;
                     if (!_buildMenuUp)
@@ -34,23 +35,53 @@ public class BuildController : MonoBehaviour {
                     }
                     ToggleBuildMenu(_buildMenuUp);
                     Debug.Log("Can spawn here");
+                    SelectedTurretSpawnLocation.transform.position = _hit.transform.position;
                 }
-                if(_hit.transform.tag == "BuildingSpawn")
+                if (_hit.transform.tag == "BuildingSpawn")
                 {
                     _selectedTurretType = _hit.transform.GetComponent<BuildInfo>().projectileTypes;
-                   
+                    
+
                     switch (_selectedTurretType)
                     {
                         case (ProjectileTypes.Fire):
-                            //Debug.Log(_buildingInfo.FireBuildingLevelsCosts[_buildingInfo.BuildingLevels["Fire"]]);
                             _gameController.CurrentSelectedBuildingCost = _buildingInfo.FireBuildingLevelsCosts[_buildingInfo.BuildingLevels["Fire"]];
                             break;
                         case (ProjectileTypes.Ice):
+                            _gameController.CurrentSelectedBuildingCost = _buildingInfo.IceBuildingLevelsCosts[_buildingInfo.BuildingLevels["Ice"]];
                             break;
                         case (ProjectileTypes.Earth):
+                            _gameController.CurrentSelectedBuildingCost = _buildingInfo.EarthBuildingLevelsCosts[_buildingInfo.BuildingLevels["Earth"]];
                             break;
                         case (ProjectileTypes.Poison):
+                            _gameController.CurrentSelectedBuildingCost = _buildingInfo.PoisonBuildingLevelsCosts[_buildingInfo.BuildingLevels["Poison"]];
                             break;
+                    }
+                }
+                if (_hit.transform.tag == "BuildButton")
+                {
+                    if (_gameController.CurrentSelectedBuildingCost <= _gameController.MoneyLeft)
+                    {
+                        switch (_selectedTurretType)
+                        {
+                            case (ProjectileTypes.Fire):
+                                Instantiate(FireTurret, CurrentTurretSpawnPoint.position, Quaternion.identity);
+                                break;
+                            case (ProjectileTypes.Ice):
+                                Instantiate(IceTurret, CurrentTurretSpawnPoint.position, Quaternion.identity);
+                                break;
+                            case (ProjectileTypes.Earth):
+                                Instantiate(EarthTurret, CurrentTurretSpawnPoint.position, Quaternion.identity);
+                                break;
+                            case (ProjectileTypes.Poison):
+                                Instantiate(PoisonTurret, CurrentTurretSpawnPoint.position, Quaternion.identity);
+                                break;
+                        }
+                        _gameController.MoneyLeft -= _gameController.CurrentSelectedBuildingCost;
+                    }
+                    else
+                    {
+
                     }
                 }
                 else
@@ -58,11 +89,11 @@ public class BuildController : MonoBehaviour {
                     _buildMenuUp = false;
                     //ToggleBuildMenu(_buildMenuUp);
                 }
-                Debug.Log(_hit.transform.tag);
+
+                }
+
             }
-            
         }
-    }
 
     void ToggleBuildMenu(bool enable)
     {
